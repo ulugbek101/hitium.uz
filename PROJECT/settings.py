8bd481e6f14fc797e7fc8d54686fee1e2c1dc077
Ulug'bek Umaraliyev <thedevu101@gmail.com>
2025-02-08 05:11:35 +0500

    Complete home page (index.html)

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -137,10 +137,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'staticfiles',
 ]
 
 MEDIA_URL = 'media/'
